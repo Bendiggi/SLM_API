888e9f808f40b236f6c71f584b327ab22950ad9f
Bendiggi@gmail.com <benrie02>
2022-06-22 14:07:08 +0200

    add ApiController and convertInch function

--- src/main/java/com/example/slm_api/SlmApiApplication.java
+++ src/main/java/com/example/slm_api/SlmApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SlmApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SlmApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SlmApiApplication.class, args);
+    }
 
 }
