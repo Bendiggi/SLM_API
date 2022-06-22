16aeebc0b47acceefdcaf45f79e812f74e0a77b4
Bendiggi@gmail.com <benrie02>
2022-06-22 13:38:00 +0200

    Initial commit

---
+++ src/main/java/com/example/slm_api/SlmApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slm_api/SlmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
